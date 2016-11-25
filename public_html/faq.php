eaea33773bead324939ea836d2a5ee95abd5d6b7
psalv <psalvato@uwo..ca>
2016-11-25 17:58:00 -0500

    Added a number of css and js files that are necessary.
    Added a basic FAQ module.

---
+++ public_html/faq.php
@@ -0,0 +1,77 @@
+
+
+<!--
+
+
+This is a basic faq module,
+the links and scripts included are needed for this module to be functional.
+
+To use this module, place the links in the header and the scripts in the footer,
+ensure you do not link the same file multiple times.
+
+
+
+TODO: add css to support this file
+
+    Classes to make:
+        - FAQ
+        - faqContent
+
+-->
+
+
+
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+
+<!-- FAQ contents goes in here -->
+<div id="faq">
+    <div class="container">
+
+
+        <div class="row">
+
+            <div class="col-sm-10 col-sm-offset-1">
+                <a href="#faq1"  data-toggle="collapse"><h2>FAQ #1</h2><hr style="width: 100%"><br></a>
+                <div id="faq1" class="collapse">
+                    <p style="font-size: 14px;">
+                        Lorem ipsum dolor sit amet, ne quas soleat interpretaris mel.
+                        Cu quo vide eruditi iracundia, has eu feugait noluisse. Ad
+                        amet recteque laboramus vim, sea nominavi legendos at. Per ut
+                        ullum imperdiet, fierent mediocrem ad sit.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-sm-10 col-sm-offset-1">
+                <a href="#faq2"  data-toggle="collapse"><h2>FAQ #2</h2><hr style="width: 100%"><br></a>
+                <div id="faq2" class="collapse">
+                    <p style="font-size: 14px;">
+                        Lorem ipsum dolor sit amet, ne quas soleat interpretaris mel.
+                        Cu quo vide eruditi iracundia, has eu feugait noluisse. Ad
+                        amet recteque laboramus vim, sea nominavi legendos at. Per ut
+                        ullum imperdiet, fierent mediocrem ad sit.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-sm-10 col-sm-offset-1">
+                <a href="#faq3"  data-toggle="collapse"><h2>FAQ #3</h2><hr style="width: 100%"><br></a>
+                <div id="faq3" class="collapse">
+                    <p style="font-size: 14px;">
+                        Lorem ipsum dolor sit amet, ne quas soleat interpretaris mel.
+                        Cu quo vide eruditi iracundia, has eu feugait noluisse. Ad
+                        amet recteque laboramus vim, sea nominavi legendos at. Per ut
+                        ullum imperdiet, fierent mediocrem ad sit.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
