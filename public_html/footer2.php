83265ea411706c3d489515135313841090b5e8fc
psalv <psalvato@uwo..ca>
2016-11-25 20:08:28 -0500

    Fixed font-awesome icons.

--- public_html/footer2.php
+++ public_html/footer2.php
@@ -1,7 +1,7 @@
 
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="assets/css/material-kit.css" rel="stylesheet"/>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
