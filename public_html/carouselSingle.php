7548fc39028f3e0da6f3b630e209e71376f1390b
psalv <WalrusMango1>
2016-11-26 12:48:57 -0500

    Added a single image carousel.

---
+++ public_html/carouselSingle.php
@@ -0,0 +1,74 @@
+
+
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+
+<div class="row">
+    <div class="card card-carousel" style="width: 50%; background-color: green">
+        <div class="carousel slide three" id="teamCarousel" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#teamCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#teamCarousel" data-slide-to="1" class=""></li>
+                <li data-target="#teamCarousel" data-slide-to="2" class=""></li>
+                <li data-target="#teamCarousel" data-slide-to="3" class=""></li>
+                <li data-target="#teamCarousel" data-slide-to="4" class=""></li>
+                <li data-target="#teamCarousel" data-slide-to="5" class=""></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="col-md-4">
+                        <a href="#">
+                            <img src="http://placehold.it/500x501" class="img-responsive">
+
+                            <!--If you want to add acaptions to the carousel do so witht hefollowing tag:-->
+                            <!--
+                            <div class="carousel-caption">
+                                <h4><i class="material-icons">location_on</i> 1</h4>
+                            </div>
+                            -->
+
+                        </a></div>
+                </div>
+                <div class="item">
+                    <div class="col-md-4">
+                        <a href="#">
+                            <img src="http://placehold.it/500x502" class="img-responsive">
+                        </a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="col-md-4">
+                        <a href="#">
+                            <img src="http://placehold.it/500x503" class="img-responsive">
+                        </a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="col-md-4">
+                        <a href="#">
+                            <img src="http://placehold.it/500x504" class="img-responsive">
+                        </a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="col-md-4">
+                        <a href="#">
+                            <img src="http://placehold.it/500x505" class="img-responsive">
+                        </a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="col-md-4">
+                        <a href="#">
+                            <img src="http://placehold.it/500x506" class="img-responsive">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <a class="left carousel-control" href="#teamCarousel" data-slide="prev"></a>
+            <a class="right carousel-control" href="#teamCarousel" data-slide="next"></a>
+        </div>
+    </div>
+</div>
