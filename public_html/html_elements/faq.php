400ff3b7f07701fe468416eed225c803b829e271
psalv <psalvato@uwo.ca>
2016-11-28 17:24:19 -0500

    To be overridden.

--- public_html/html_elements/faq.php
+++ public_html/html_elements/faq.php
@@ -1,8 +1,4 @@
 
-<link href="https://fonts.googleapis.com/css?family=Vidaloka" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
-<!--<link rel="stylesheet" href="assets/css/style.css">-->
 
 <!-- FAQ contents goes in here -->
 <div id="faq">
