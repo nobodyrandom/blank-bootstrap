5d40a851dfe262b64daf8af28fa23a0a3485b53a
psalv <psalvato@uwo..ca>
2016-11-27 20:00:06 -0500

    CSS changes, added footer3

--- public_html/footer2.php
+++ public_html/footer2.php
@@ -1,3 +1,4 @@
+<link href="https://fonts.googleapis.com/css?family=Vidaloka" rel="stylesheet">
 
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="assets/css/style.css">
