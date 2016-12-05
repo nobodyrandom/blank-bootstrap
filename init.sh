33503e4873086e535438ca1a0a8269a49b365fd5
Alex Madrzyk <amadrzyk@berkeley.edu>
2016-12-05 17:59:55 -0500

    Added brew and php install functionality to shell script

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 RED=$'\033[0;31m'
 NC=$'\033[0m' # No Color
+BREW=0 # Brew and Xcode installed?
+XCODE=0
 
 echo "Why you so lazy >.<"
 sleep 1
@@ -15,7 +17,7 @@ echo "Make sure to include the username and slash"
 read REPO
 
 while true; do
-    read -p "Using ${RED}https://github.com/$REPO.git${NC} Are you sure this is correct?" yn
+    read -p "Using ${RED}https://github.com/$REPO.git${NC} Are you sure this is correct? " yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) echo "I give up, run da script to try again."; exit;;
@@ -33,6 +35,58 @@ echo -e "Setting up API remote for api sync"
 git remote add api https://github.com/nobodyrandom/blank-bootstrap.git
 
 echo "Alls well. Done init."
+
+# Ask about installing brew
+while true; do
+    read -p "Would you like brew installed on MacOS? (It'll help us install php later) " yn
+    case $yn in
+        [Yy]* )
+            echo "Please ensure Xcode is installed on your mac."
+            echo "xcode-select --install";
+            xcode-select --install;
+            echo "/usr/bin/ruby -e $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+            break;;
+        [Nn]* ) echo "Great, we won't install it then."; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Check if brew is installed
+if [ `which brew | wc -m` != 0 ]; then
+    # Brew is installed!
+    BREW=1;
+else
+    # Brew is not installed
+    echo "If you would like this script to install php, please install brew first."
+fi
+
+# Check if Xcode is installed
+if [ `find /Applications -maxdepth 1 -name Xcode.app | wc -m` != 0 ]; then
+    # Xcode is installed!
+    XCODE=1;
+else
+    # Xcode is not installed
+    echo "If you would like this script to install php, please install Xcode first."
+fi
+
+sleep 1
+
+while [[ BREW -eq 1 && XCODE -eq 1 ]]; do
+    read -p "Shall we install php on your Mac? " yn
+    case $yn in
+        [Yy]* ) echo "Future tip: If you run into problems, try updating Xcode Command Line Tools.";
+            brew tap homebrew/dupes;
+            brew tap homebrew/versions;
+            brew tap homebrew/homebrew-php;
+            brew install php70;
+            break;;
+        [Nn]* ) echo "Php will not be set up."; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+sleep 1
 
 while true; do
     read -p "Do you want to init composer as well? (php must be setup for this to work)" yn
