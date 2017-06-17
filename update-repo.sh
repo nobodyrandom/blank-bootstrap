b7dc013dea2835fad2f4c632a4d6f773ac9ee156
Nobody Random <iamnobodyrandom@yahoo.com>
2017-06-17 13:35:48 +0800

    ask true

--- update-repo.sh
+++ update-repo.sh
@@ -3,5 +3,15 @@
 git fetch api
 
 echo "Warning some local files may be overridden"
+sleep 1
+
+while true; do
+    read -p "Do you want to continue? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "I give up, run da script to try again."; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 git pull api master
